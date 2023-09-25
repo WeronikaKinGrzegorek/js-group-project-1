cccd58dd61daebcd9e0484454f4ef97318466a13
MichalSzmajduch <m.szmajduch1996@gmail.com>
2023-09-25 21:16:08 +0200

    modal-close
    prawie działający Esc

---
+++ src/js/modal-close.js
@@ -0,0 +1,33 @@
+const modal = document.getElementById('movieModal');
+const openBtn = document.getElementById('movieModal');
+
+function openModal() {
+  modal.style.display = 'block';
+  modal.focus();
+
+  document.addEventListener('keydown', closeModalOnEsc);
+
+  window.addEventListener('click', closeModalOnClickOutside);
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+
+  document.removeEventListener('keydown', closeModalOnEsc);
+
+  window.removeEventListener('click', closeModalOnClickOutside);
+}
+
+function closeModalOnEsc(event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+}
+
+function closeModalOnClickOutside(event) {
+  if (event.target === modal) {
+    closeModal();
+  }
+}
+
+openBtn.addEventListener('click', openModal);
