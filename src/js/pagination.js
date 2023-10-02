83bf7a592b3732f41ba4d8a9e78889216eedbd83
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-10-02 21:55:55 +0200

    paginacja jeszcze raz

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,12 +1,11 @@
 
 let currentPage = 1
 const pageSize = 18
-let inputValue = ''
 
 export async function loadMoreMovies(drawMoviesFunction, inputValue) {
     try {
         currentPage++
-        await drawMoviesFunction(inputValue, currentPage, pageSize)
+      await drawMoviesFunction(inputValue, currentPage, pageSize)
     } catch (error) {
         console.log(error)
     }
