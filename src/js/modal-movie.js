c95109a0cd5fe6d7043aa70e787865a1912bbea2
Stefan Bielak <117030658+StefanBielak@users.noreply.github.com>
2023-10-03 19:42:42 +0200

    ready???

--- src/js/modal-movie.js
+++ src/js/modal-movie.js
@@ -1,12 +1,9 @@
-
 import { addToWatchlist } from './add-watchlist.js';
 import { addToQueue } from './add-queue.js';
-
 import { drawMovies } from './draw-movie.js';
 import { getFilmDetails } from './fetch.js';
 
 const modal = document.getElementById('movieModal');
-
 const modalPoster = modal.querySelector('#modalPoster');
 const modalTitle = modal.querySelector('#modalTitle');
 const modalRating = modal.querySelector('#modalRating');
@@ -22,6 +19,9 @@ const trailerLink = modal.querySelector('#trailerLink');
 const BASE_POSTER_PATH = 'https://image.tmdb.org/t/p/w500';
 
 let movieData;
+let trailerIframe; // Zmienna do przechowywania referencji do iframe z trailerem
+let trailerVideoId; // Zmienna do przechowywania ID wideo trailera
+
 
 document.addEventListener('DOMContentLoaded', function () {
   let currentPage = 1;
@@ -86,7 +86,34 @@ function que() {
 function watched() {
   addToWatchlist(movieData);
 }
+trailerLink.addEventListener('click', async () => {
+  try {
+    const apiKey = 'AIzaSyBIIbR4hGJHy_kutGiTF8pwZi7a93ZFrU0'; // Zastąp 'TWÓJ KLUCZ API Z YOUTUBE' własnym kluczem API z YouTube
+    const youtubeApiUrl = `https://www.googleapis.com/youtube/v3/search?type=video&q=${movieData.title} trailer&key=${apiKey}&maxResults=1`;
 
+    const response = await fetch(youtubeApiUrl);
+    const data = await response.json();
+
+    if (data.items && data.items.length > 0) {
+        const trailerKey = data.items[0].id.videoId;
+
+        // Twórz <iframe> z odpowiednimi stylami
+        const trailerIframe = document.createElement('iframe');
+        trailerIframe.id = 'trailerIframe';
+        trailerIframe.src = `https://www.youtube.com/embed/${trailerKey}`;
+        trailerIframe.allowfullscreen = true;
+
+        // Wstaw <iframe> do modala
+        modal.innerHTML = '';
+        modal.appendChild(trailerIframe);
+        modal.style.display = 'flex'; // Użyj flexbox do centrowania modala
+      } else {
+        alert('Nie znaleziono trailera dla tego filmu.');
+      }
+    } catch (error) {
+      console.error(error);
+  }
+});
 
 function closeModal() {
   modal.style.display = 'none';
