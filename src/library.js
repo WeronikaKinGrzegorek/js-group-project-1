c0d47337aec42c21c6b7eac91e7229b2be2bdf25
WeronikaKinGrzegorek <126391598+WeronikaKinGrzegorek@users.noreply.github.com>
2023-10-03 22:10:05 +0200

    added implementing watched movies to library page

--- src/library.js
+++ src/library.js
@@ -6,12 +6,16 @@ import './js/add-queue';
 import './js/add-watchlist';
 import './js/watchedLibrary';
 import './js/queueLibrary';
+import { showLoader, hideLoader } from './js/loader.js';
 
 import './js/modal-movie';
 
-import { drawMovies } from './js/draw-movie.js';
-import { displayWatchedMovies } from './js/queueLibrary';
+import { displayWatchedMovies } from './js/watchedLibrary.js';
 
-drawMovies(displayWatchedMovies);
+const watchedMovies = JSON.parse(localStorage.getItem('movieWatchlist')) || [];
 
-
+document.addEventListener('DOMContentLoaded', function () {
+  showLoader();
+  displayWatchedMovies(watchedMovies);
+  hideLoader();
+});
