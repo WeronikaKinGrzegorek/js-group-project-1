3afb5ca785d4c9a2e6a29448c79a53a80bacb732
Stefan Bielak <117030658+StefanBielak@users.noreply.github.com>
2023-10-03 22:42:15 +0200

    fiiiinish

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -1,7 +1,7 @@
 export async function openYoutubeTrailer(movieData) {
   try {
     const movieTitleForSearch = encodeURIComponent(movieData.title);
-    const apiKey = 'AIzaSyBIIbR4hGJHy_kutGiTF8pwZi7a93ZFrU0'; // Podmień to na swój klucz API
+    const apiKey = 'AIzaSyBGEBT2TOthR5IeC42NMx-jS-6ozWXTgmM'; // Podmień to na swój klucz API
     const apiEndpoint = `https://www.googleapis.com/youtube/v3/search?q=${movieTitleForSearch}+trailer&key=${apiKey}&type=video`;
 
     const response = await fetch(apiEndpoint);
