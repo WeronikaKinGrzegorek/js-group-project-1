04db0d9796f76f377c2cefe3b1f8d8e016985712
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2023-10-04 13:10:48 +0200

    this is all about to delete library gallery
    change status of button, delete queued films from queue gallery, plus few imports between modal-movie.js and add-queue.js

--- src/js/add-queue.js
+++ src/js/add-queue.js
@@ -1,5 +1,6 @@
 import { Notify } from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
+import { modal, watchlistButton } from './modal-movie';
 
 const savedMovies = JSON.parse(localStorage.getItem('movieQueue')) || [];
 
@@ -21,4 +22,30 @@ export function addToQueue(movieData) {
     Notify.failure(`Movie "${movieData.title}" is already in queue list.`);
   }
 }
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+export function isMovieInQueue(movieData) {
+  const movieId = movieData.id;
+  return savedMovies.some(movieInQueue => movieInQueue.id === movieId);
+}
 
+export function removeFromQueue(movieData) {
+  const movieId = movieData.id;
+  const isMovieInQueue = savedMovies.some(movieInQueue => movieInQueue.id === movieId);
+
+
+  if (isMovieInQueue) {
+    const movieIndex = savedMovies.findIndex(movie => movie.id === movieId);
+    savedMovies.splice(movieIndex, 1);
+    localStorage.setItem('movieQueue', JSON.stringify(savedMovies));
+    Notify.success(`Removed movie "${movieData.title}" from queue list.`);
+
+    // Zaktualizuj przycisk w modalu na "Add to Queue"
+    const watchlistButton = document.querySelector('#watchlistButton');
+    if (watchlistButton) {
+      watchlistButton.textContent = 'Add to Queue';
+    }
+  } else {
+    Notify.failure(`Movie "${movieData.title}" is not in the queue.`);
+  }
+}
+//~~~~~~~~~~~~~~~~~~~~~
